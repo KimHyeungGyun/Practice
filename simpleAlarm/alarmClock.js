fe3ae602ed841314f2111371c2acdbcbb91657fb
KimHyeungGyun <gudrbsdlwid@gmail.com>
2020-11-21 12:14:09 +0900

    Add simpleAlarm practice
    심심풀이

---
+++ simpleAlarm/alarmClock.js
@@ -0,0 +1,6 @@
+let timer = document.getElementById("timer");
+let hours = document.getElementById("timer");
+let timer = document.getElementById("timer");
+let timer = document.getElementById("timer");
+let timer = document.getElementById("timer");
+let timer = document.getElementById("timer");
