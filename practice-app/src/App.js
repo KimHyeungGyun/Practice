046b682491213975499ce7140104ca9d010bbf0f
KimHyeungGyun <gudrbsdlwid@gmail.com>
2020-11-29 20:45:47 +0900

    Complete Simple Movie App
    간단 영화 페이지

--- practice-app/src/App.js
+++ practice-app/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
-import Movie from './Movies';
+import Movie from './Movie';
+import './App.css';
 
 class App extends React.Component {
   constructor(props) {
@@ -35,19 +36,20 @@ class App extends React.Component {
             <span className="loader__text">Loading...</span>
           </div>
         ) : (
-          <div className="movie">
-            {movies.map((movie) => (
-              <Movie
-                key={movie.id}
-                id={movie.id}
-                year={movie.year}
-                title={movie.title}
-                summary={movie.summary}
-                poster={movie.medium_cover_image}
-              />
-            ))}
-          </div>
-        )}
+            <div className="movies">
+              {movies.map((movie) => (
+                <Movie
+                  key={movie.id}
+                  id={movie.id}
+                  year={movie.year}
+                  title={movie.title}
+                  summary={movie.summary}
+                  poster={movie.medium_cover_image}
+                  genres={movie.genres}
+                />
+              ))}
+            </div>
+          )}
       </section>
     );
   }
--- practice-app/src/Movie.js
+++ practice-app/src/Movie.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './Movie.css';
 
 function Movie({
-  id, year, title, summary, poster,
+  id, year, title, summary, poster, genres,
 }) {
   return (
     <div className="movie">
@@ -10,7 +11,17 @@ function Movie({
       <div className="movie__data">
         <h3 className="movie__title">{title}</h3>
         <h5 className="movie__year">{year}</h5>
-        <p className="movie__summary">{summary}</p>
+        <ul className="movie__genres">
+          {genres.map((genre, index) => (
+            <li key={index} className="genres__genre">
+              {genre}
+            </li>
+          ))}
+        </ul>
+        <p className="movie__summary">
+          {summary.slice(0, 180)}
+          ...
+        </p>
       </div>
     </div>
   );
@@ -22,6 +33,7 @@ Movie.propTypes = {
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
   poster: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 
 export default Movie;
