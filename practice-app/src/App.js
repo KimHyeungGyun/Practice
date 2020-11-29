6a6fe4229db6b148722171e45e8a94bf67f8c7e7
KimHyeungGyun <gudrbsdlwid@gmail.com>
2020-11-29 21:12:02 +0900

    add simple Navigation
    간단한 네비 추가

--- practice-app/src/App.js
+++ practice-app/src/App.js
@@ -1,58 +1,18 @@
 import React from 'react';
-import axios from 'axios';
-import Movie from './Movie';
+import { HashRouter, Route } from 'react-router-dom';
+import Home from './routes/Home';
+import About from './routes/About';
+import Navigation from './components/Navigation';
 import './App.css';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isLoading: true,
-      movies: [],
-    };
-  }
-
-  componentDidMount() {
-    this.getMovies();
-  }
-
-  getMovies = async () => {
-    const {
-      data: {
-        data: { movies },
-      },
-    } = await axios.get('https://yts-proxy.nomadcoders1.now.sh/list_movies.json');
-    this.setState({ movies, isLoading: false });
-    // console.log(movies);
-  }
-
-  render() {
-    const { isLoading, movies } = this.state;
-
-    return (
-      <section className="container">
-        {isLoading ? (
-          <div className="loader">
-            <span className="loader__text">Loading...</span>
-          </div>
-        ) : (
-            <div className="movies">
-              {movies.map((movie) => (
-                <Movie
-                  key={movie.id}
-                  id={movie.id}
-                  year={movie.year}
-                  title={movie.title}
-                  summary={movie.summary}
-                  poster={movie.medium_cover_image}
-                  genres={movie.genres}
-                />
-              ))}
-            </div>
-          )}
-      </section>
-    );
-  }
+function App() {
+  return (
+    <HashRouter>
+      <Navigation />
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/about" component={About} />
+    </HashRouter>
+  );
 }
 
 export default App;
---
+++ practice-app/src/routes/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './About.css';
+
+function About() {
+  return (
+    <div className="about__container">
+      <span>
+        “Simple Movie App.”
+      </span>
+      <span>− Kim </span>
+    </div>
+  );
+}
+
+export default About;
